82c4b12c57a789f6014e7bce854781d32899434d
lcharollais <louis.charollais@gmail.com>
2023-10-24 18:50:33 +0200

    ajout d'une page d'affichage des détails d'un album dans easyadmin

--- src/Controller/Admin/AlbumCrudController.php
+++ src/Controller/Admin/AlbumCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Album;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -14,15 +17,22 @@ class AlbumCrudController extends AbstractCrudController
         return Album::class;
     }
 
-
     public function configureFields(string $pageName): iterable
     {
         return [
             //IdField::new('id'),
             TextField::new('nom'),
-            AssociationField::new('membres'),
+            AssociationField::new('membre'),
             AssociationField::new('paninis'),
             //TextEditorField::new('description')
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ;
     }
 }
