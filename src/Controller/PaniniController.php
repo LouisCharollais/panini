e4f56be9580dbabca7505df293bfa4fa7fc9650f
lcharollais <louis.charollais@gmail.com>
2023-11-07 20:18:29 +0100

    ajout de la navigation vers la liste des paninis

--- src/Controller/PaniniController.php
+++ src/Controller/PaniniController.php
@@ -27,4 +27,15 @@ class PaniniController extends AbstractController
             'controller_name' => 'PaniniController'
         ]);
     }
+
+    #[Route('/panini', name: 'panini_all')]
+    public function index_bis(ManagerRegistry $doctrine): Response
+    {
+        $entity_manager = $doctrine->getManager();
+        $paninis = $entity_manager->getRepository(Panini::class)->findAll();
+
+        return $this->render('panini/index.html.twig', [
+            'paninis' => $paninis,
+        ]);
+    }
 }
