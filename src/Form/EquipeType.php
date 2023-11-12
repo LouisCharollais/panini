5df31107bb00b659c46b0af9dd28f26dd0f40e5f
lcharollais <louis.charollais@gmail.com>
2023-11-12 17:34:38 +0100

    Cohérence de la charte graphique

--- src/Form/EquipeType.php
+++ src/Form/EquipeType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Equipe;
+use App\Repository\PaniniRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -11,11 +13,27 @@ class EquipeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        //dump($options);
+        $equipe = $options['data'] ?? null;
+        $membre = $equipe->getCreateur();
+
         $builder
-            ->add('nom')
-            ->add('paninis')
-            ->add('createur')
-        ;
+            ->add('createur', null, [
+                'disabled'   => true,
+            ])
+            ->add('Paninis', EntityType::class, [
+                'class' => 'App\Entity\Panini',
+                'choice_label' => 'nom',
+                'multiple' => true,
+                'query_builder' => function (PaniniRepository $er) use ($membre) {
+                                                return $er->createQueryBuilder('o')
+                                                    ->leftJoin('o.album', 'i')
+                                                    ->andWhere('i.membre = :membre')
+                                                    ->setParameter('membre', $membre)
+                                                    ;
+                                                }
+                                                ])
+                ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
